7877de9ae88da041c803e2cece24295c8e55e230
Anna Turkiewicz <turkiewicz293@gmail.com>
2024-02-23 22:00:59 +0000

    Count number of products

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,5 +2,9 @@ shopping_list = {'piekarnia': ['chleb', 'paczek', 'bulka'],
                 'warzywniak': ['marchew', 'seler', 'rukola'],
                 'drogeria': ['szampon', 'krem']}
 
+numb_prod = 0
 for shop, list in shopping_list.items():
     print(f"Ide do {shop.capitalize()} i kupuje {[l.capitalize() for l in list]}")
+    numb_prod += len(list)
+
+print(f"W sumie kupuje {numb_prod} produktow.")
