f5b54849acc01c91a3e808895df4de664228ff69
Anna Turkiewicz <turkiewicz293@gmail.com>
2024-02-23 21:56:28 +0000

    Create for loop

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,3 +1,6 @@
 shopping_list = {'piekarnia': ['chleb', 'paczek', 'bulka'],
                 'warzywniak': ['marchew', 'seler', 'rukola'],
                 'drogeria': ['szampon', 'krem']}
+
+for shop, list in shopping_list.items():
+    print(f"Ide do {shop.capitalize()} i kupuje {[l.capitalize() for l in list]}")
